cb8a83be99280b5b0e8546f84ff192e7695553b4
Knight-Thomas <thomasmilesknight@icloud.com>
2024-03-04 09:43:20 +0000

    Connecting the login.ui to the Login-App.py

--- Login-App.py
+++ Login-App.py
@@ -9,4 +9,18 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         '''constructor method'''
         super(Ui, self).__init__()
-        uic.loadUi()
+        uic.loadUi('/Users/tomknight/GUI-programming/PyQtGUI/QtGUI/login.ui', self)
+
+        #add event listeners
+
+        #show the window
+        self.show()
+
+def mainAppication():
+    '''Main application load the window instance'''
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    sys.exit(app.exec_())
+
+mainAppication()
